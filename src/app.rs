55993dcad35b80960da02d87e496289259f04fed
Tyrese Luo <tyreseluo@outlook.com>
2024-11-15 21:24:37 +0800

    init: initial commit

---
+++ src/app.rs
@@ -0,0 +1,45 @@
+use makepad_widgets::*;
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    App = {{App}} {
+        ui: <Root> {
+            main_window = <Window> {
+                body = <ScrollXYView>{
+
+                    flow: Down,
+                    spacing:10,
+                    align: {
+                        x: 0.5,
+                        y: 0.5
+                    },
+                    button = <Button> {
+                        text: "Hello World",
+                    }
+                }
+            }
+        }
+    }
+}
+
+app_main!(App);
+
+#[derive(Live, LiveHook)]
+pub struct App {
+    #[live]
+    ui: WidgetRef,
+}
+
+impl LiveRegister for App {
+    fn live_register(cx:&mut Cx) {
+        makepad_widgets::live_design(cx);
+    }
+}
+
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.ui.handle_event(cx, event, &mut Scope::empty());
+    }
+}
